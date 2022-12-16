a263c535c32d1331029ba2c2baa99e0ae1628916
Isaiah Kimes <isakimes@gmail.com>
2022-12-16 00:32:39 -0500

    Add YunJin to home page because YunJin cute

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
-<div class="flex justify-center items-center h-full min-h-[95vh]">
+<script>
+  import YunJin from '$lib/yunjin-look.webp';
+</script>
+<div class="flex flex-wrap justify-center items-center h-full min-h-[95vh]">
+  <img src={YunJin} alt="Yunjin look" />
   <h1 class="text-white text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold decoration-slate-900 underline underline-offset-4">Welcome to Genius Invokation!</h1>
 </div>
